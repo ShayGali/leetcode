86d5778aa70c122d8b69461fec9b3aa3afbfbab2
Shay Gali <86621168+ShayGali@users.noreply.github.com>
2022-08-25 17:26:16 +0300

    344. Reverse String

---
+++ algorithms/easy/reverse_string_344/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void reverseString(char[] s) {
+        char t;
+        for(int i =0; i<s.length/2;i++){
+            t = s[i];
+            s[i] = s[s.length-1-i];
+            s[s.length-1-i] = t;
+        }
+    }
+}
