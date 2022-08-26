4a2e1beed09e65f7d6b3f22274b68fade691f542
ShayGali <shayali100@gmail.com>
2022-08-26 16:48:47 +0300

    27. Remove Element

--- algorithms/easy/remove_element_27/Solution.java
+++ algorithms/easy/remove_element_27/Solution.java
@@ -1,12 +1,25 @@
 package algorithms.easy.remove_element_27;
 
 class Solution {
+
     public int removeElement(int[] nums, int val) {
-        int r = nums.length - 1;
-        while (r >= 0 && nums[r] != val) r--;
+        int k = 0;
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) k++;
+            else {
+                swap(nums, i, i - k);
+                ans++;
+            }
 
-        int count = 0;
+        }
+        return ans;
+    }
 
-        return count;
+
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
