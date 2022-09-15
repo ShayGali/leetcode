e9ccdf37b5e364a1bda538eb5ad852e4105383d8
Shay Gali <86621168+ShayGali@users.noreply.github.com>
2022-09-15 09:27:06 +0300

    1304. Find N Unique Integers Sum up to Zero

---
+++ algorithms/easy/find_n_unique_integers_sum_up_to_zero_1304/Solution.java
@@ -0,0 +1,13 @@
+
+package algorithms.easy.1304.find_n_unique_integers_sum_up_to_zero_1304
+
+class Solution {
+    public int[] sumZero(int n) {
+        int[] res = new int[n];
+        for(int i = 0; i<n/2;i++){
+            res[i] = i+1;
+            res[n-i-1] = -(i+1);
+        }
+        return res;
+    }
+}
