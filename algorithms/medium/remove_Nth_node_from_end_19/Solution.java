3d29bb764ba24f8643fcc15c64511ee30195655c
ShayGali <shayali100@gmail.com>
2022-09-28 21:39:21 +0300

    19. Remove Nth Node From End of List

--- algorithms/medium/remove_Nth_node_from_end_19/Solution.java
+++ algorithms/medium/remove_Nth_node_from_end_19/Solution.java
@@ -42,27 +42,47 @@ public class Solution {
 //        return head.next;
 //    }
 
+//    public ListNode removeNthFromEnd(ListNode head, int n) {
+//        ListNode temp = head;
+//        ListNode temp2 = head;
+//        int counter = 0;
+//        while (temp != null) {
+//            if (counter++ > n) temp2 = temp2.next;
+//            temp = temp.next;
+//        }
+//        if (temp2 == null) return null;
+//        if (temp2 == head) return head.next;
+//        if (temp2.next == null)
+//            return null;
+//        temp2.next = temp2.next.next;
+//        return head;
+//    }
+
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode temp = head;
-        ListNode temp2 = head;
-        int counter = 0;
-        while (temp != null) {
-            if (counter++ > n) temp2 = temp2.next;
-            temp = temp.next;
-        }
-        if (temp2 == null) return null;
-        if (temp2 == head) return head.next;
-        if (temp2.next == null)
+        if (head == null || head.next == null) {
             return null;
-        temp2.next = temp2.next.next;
+        }
+        ListNode slow = head, fast = head;
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+        if (fast == null)
+            return head.next;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
         return head;
     }
+
 
     public static void main(String[] args) {
 //        ListNode l = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
         ListNode l = new ListNode(1, new ListNode(2));
 //        ListNode l = new ListNode(1);
 //        System.out.println(l);
-        System.out.println(new Solution().removeNthFromEnd(l, 1));
+        System.out.println(new Solution().removeNthFromEnd(l, 2));
     }
 }
