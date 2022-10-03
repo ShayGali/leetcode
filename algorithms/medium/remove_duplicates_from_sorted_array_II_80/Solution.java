1c8fbc6ae4504c68beee8f31d1359009ed165568
ShayGali <shayali100@gmail.com>
2022-10-03 22:37:48 +0300

    80. Remove Duplicates from Sorted Array II

--- algorithms/medium/remove_duplicates_from_sorted_array_II_80/Solution.java
+++ algorithms/medium/remove_duplicates_from_sorted_array_II_80/Solution.java
@@ -1,7 +1,7 @@
 package algorithms.medium.remove_duplicates_from_sorted_array_II_80;
 
 public class Solution {
-    public int removeDuplicates(int[] nums) {
+    public int removeDuplicates1(int[] nums) {
         if (nums.length == 1) return 1;
         int last = 1;
         for (int i = 0; i < nums.length; i++) {
@@ -13,6 +13,19 @@ public class Solution {
         return last + 1;
     }
 
+    public int removeDuplicates2(int[] nums) {
+        if (nums.length < 3) return nums.length;
+        int last = 2;
+        for (int i = 2; i < nums.length; i++)
+            if (nums[i] != nums[last - 2])
+                nums[last++] = nums[i];
+
+        return last;
+    }
+
+    public static void main(String[] args) {
+        new Solution().removeDuplicates2(new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3});
+    }
 }
 
 /*
