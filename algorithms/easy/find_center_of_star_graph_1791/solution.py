64437d00be716e1c4d83d7033857f0fb6333daa3
ShayG <86621168+ShayGali@users.noreply.github.com>
2024-06-27 19:47:17 +0300

    1791. Find Center of Star Graph

--- algorithms/easy/find_center_of_star_graph_1791/solution.py
+++ algorithms/easy/find_center_of_star_graph_1791/solution.py
@@ -1,14 +1,6 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        counter = {}
-        for e in edges:
-            u = counter.get(e[0],0)
-            v = counter.get(e[1],0)
-            if u == 1:
-                return e[0]
-            if v == 1:
-                return e[1]
-            
-            counter[e[0]] = u + 1
-            counter[e[1]] = v + 1
+        if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]:
+            return edges[0][0]
+        return edges[0][1]
         
